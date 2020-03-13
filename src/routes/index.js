c6b87426aac1a24b1e9074719888e655b4f4d36f
PoOwAa <raycsucsu@gmail.com>
2020-03-13 18:08:14 +0100

    url validation server-side (need clientSide as well!)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const UrlHandler = require("../services/url");
 const title = "Url Shortener";
+const validator = require("validator");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -12,6 +13,10 @@ router.post("/shortUrl", async function(req, res, next) {
   try {
     if (!req.body.longUrl) {
       throw new Error("There is nothing to shorten!");
+    }
+
+    if (!validator.isURL(req.body.longUrl)) {
+      throw new Error(`Sorry, this is not a valid url!`);
     }
     const url = await UrlHandler.save(req.body.longUrl);
     res.render("pages/saved", { url, title, hostname: req.headers.origin });
